cbdca444e4896463b2e1648fc3d5360a24f56eda
SoHyeon Kim <thgus7772@naver.com>
2024-02-21 01:08:43 +0900

    3rd commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const API_KEY = `8ca46f5c8b624a54badf9b8ccc56354c`;
 let news = [];
 const getLatestNews = async () => {
-    const url = new URL(`http://sh-times.netlify.app/top-headlines?category=science`);
+    const url = new URL(`https://sh-times.netlify.app/top-headlines`);
     const response = await fetch(url);
     const data = await response.json();
     news = data.articles;
