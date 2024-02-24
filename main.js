179701d5b714bb25867520eda9f6883e969158d0
SohyeonKim <74132692+thgusKim@users.noreply.github.com>
2024-02-24 23:58:34 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,16 @@ const API_KEY = `8ca46f5c8b624a54badf9b8ccc56354c`;
 let newsList = [];
 const menus = document.querySelectorAll(".menus button");
 menus.forEach(menu => menu.addEventListener("click", (event) => getNewsByCategory(event)));
-let url = new URL(`https://sh-times.netlify.app/top-headlines`);
+let url = new URL(`https://sh-times.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`);
+let totalResults = 0;
+let page = 1;
+const pageSize = 10;
+const groupSize = 5;
 
 const getNews = async () => {
     try {
+        url.searchParams.set("page", page);
+        url.searchParams.set("pageSize", pageSize);
         const response = await fetch(url);
         const data = await response.json();
         if (response.status === 200) {
@@ -13,7 +19,9 @@ const getNews = async () => {
                 throw new Error("No result for this search");
             }
             newsList = data.articles;
+            totalResults = data.totalResults;
             render();
+            paginationRender();
         } else {
             throw new Error(data.message);
         }
@@ -24,7 +32,7 @@ const getNews = async () => {
 }
 const getLatestNews = async () => {
     //const url = new URL(`https://sh-times.netlify.app/top-headlines`);
-    url = new URL(`https://sh-times.netlify.app/top-headlines`);
+    url = new URL(`https://sh-times.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`);
     getNews();
 }
 
@@ -72,7 +80,6 @@ const openSearchBox = () => {
     }
 };
 
-getLatestNews();
 
 const errorRender = (errorMessage) => {
     const errorHTML = `<div class="alert alert-danger" role="alert">
@@ -80,3 +87,28 @@ const errorRender = (errorMessage) => {
   </div>`;
     document.getElementById("news-board").innerHTML = errorHTML;
 }
+
+const paginationRender = () => {
+    const totalPages = Math.ceil(totalResults / pageSize);
+    const pageGroup = Math.ceil(page / groupSize);
+    let lastPage = pageGroup * groupSize;
+    if (lastPage > totalPages) {
+        lastPage = totalPages;
+    }
+
+    const firstPage = lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
+    let paginationHTML = ``;
+
+    for (let i = firstPage; i <= lastPage; i++) {
+        paginationHTML += `<li class="page-item ${i === page ? "active" : ""}" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`;
+    }
+
+    document.querySelector(".pagination").innerHTML = paginationHTML;
+}
+
+const moveToPage = (pageNum) => {
+    page = pageNum;
+    getNews();
+}
+
+getLatestNews();
